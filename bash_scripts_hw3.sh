8005b133fc737070a191131471b66f993e0a9d0b
Marharita Romaniuk <puma28purple@gmail.com>
2022-09-11 16:26:30 +0300

    Chenge coments

--- bash_scripts_hw3.sh
+++ bash_scripts_hw3.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 #script does the following:
-##writes "Creating folder" to console
-##creates a folder called Name_Surname
-##lists folders in current directory
-##writes "Creating files" to console
-##craetes two files called Name and Surname
-##lists contents of Name_Surname folder
+#writes "Creating folder" to console
+#creates a folder called Name_Surname
+#lists folders in current directory
+#writes "Creating files" to console
+#craetes two files called Name and Surname
+#lists contents of Name_Surname folder
 
 echo creating folder
 mkdir Rita_Romaniuk
