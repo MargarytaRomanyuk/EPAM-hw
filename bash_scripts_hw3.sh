b8bc6340f7f7b3fb868993afcd74eeaa940b07f4
MargarytaRomanyuk <106387414+MargarytaRomanyuk@users.noreply.github.com>
2022-09-11 14:22:14 +0300

    add coments

--- bash_scripts_hw3.sh
+++ bash_scripts_hw3.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+#script does the following:
+##writes "Creating folder" to console
+##creates a folder called Name_Surname
+##lists folders in current directory
+##writes "Creating files" to console
+##craetes two files called Name and Surname
+##lists contents of Name_Surname folder
+
 echo creating folder
 mkdir Rita_Romaniuk
 ls
